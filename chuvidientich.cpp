9f766a8af188974f528fedb745b89fc065f4159e
caoletuanvu <caoletuanvu1610@gmail.com>
2020-10-23 11:40:11 +0700

    Hinh chu nhat

--- chuvidientich.cpp
+++ chuvidientich.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int dienTichVuong();
-int chuViVuong();
-int chuViCNhat();
-int dienTichCNhat();
-int dienTichTron();
-int chuViTron():
+
+int chuViCNhat(int dai, int rong);
+int dienTichCNhat(int dai, int rong);
+
+
+int dienTichCNhat(int dai, int rong)
+{
+	return a*b;
+}
+int chuViCNhat(int dai, int rong)
+{
+	return (a+b)*2;
 
 int main()
 {
+int dai;
+	int rong;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
+
+
+	cout << "Nhap vao chieu dai:";
+	cin >> dai;
+	cout << "Nhap vao chieu rong:";
+	cin >> rong;
+	
+	cout << "chuViCNhat " << dai << " va " << rong << " la: " << chuViCNhat(dai, rong);
+	cout << "dienTichCNhat " << dai << " va " << rong << " la: " << chuViCNhat(dai, rong);
 
 return 0;
 }
