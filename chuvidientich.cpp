b5dbe943ee152616acdc96e48f23d1cde36269fd
Truong Ngoc Khanh <khanhert@gmail.com>
2020-10-23 11:28:37 +0700

    Source-a

---
+++ chuvidientich.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int dienTichVuong();
+int chuViVuong();
+int chuViCNhat();
+int dienTichCNhat();
+int dienTichTron();
+int chuViTron():
+
+int main()
+{
+
+
+return 0;
+}
