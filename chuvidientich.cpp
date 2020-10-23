8023445677b6af4e9a5f7c1da85be5603750a1b3
nhung2408 <nttnhunga4tqtpy@gmail.com>
2020-10-23 11:36:16 +0700

    Hinh Tron

--- chuvidientich.cpp
+++ chuvidientich.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <math.h>
+#define PI 3.14159
 using namespace std;
 
-int dienTichVuong();
-int chuViVuong();
-int chuViCNhat();
-int dienTichCNhat();
-int dienTichTron();
-int chuViTron():
 
+float dienTichTron(int b);
+float chuViTron(int b);
+
+float chuViHinhTron(int b){
+	return 2*PI*b;
+
+}
+
+float dienTichHinhTron(int b){
+	return PI*b*b;
+}
 int main()
 {
-
-
+int b;
+cout<<"Nhap vao ban kinh hinh tron :";
+cin>>b;
+cout<<"Chu vi hinh tron la : "<<chuViHinhTron(b);
+cout<<"\nDien tich hinh tron la : "<<dienTichHinhTron(b)<<endl;
+system("pause");
 return 0;
+
+
 }
