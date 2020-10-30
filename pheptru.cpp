50fa22265dfaf9cafe814614918f5fee80933f3d
Khanh <khanhert@gmail.com>
2020-10-30 11:34:02 +0700

    Sua pheptru.cpp

---
+++ pheptru.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+
+
+
+
+
+//TinhHieu
+int capso(int a,int b);
+
+
+
+int tru(int a)
+{
+	int a=rand(0,100);
+	int b=rand(0,100);
+	return a-b;
+	 
+}
+int main()
+{
+	int a;
+	int b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout<<"Hieu cua << a<< va <<<b "la:"<<tru();
+	
+
+
+	
+
+	system("pause");
+	return 0;
+}
